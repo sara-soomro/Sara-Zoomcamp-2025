1d99fc372286192c11c5fa12b7ce06560479b5c5
Sara Hassan Soomro <soomro.sara0@gmail.com>
2025-02-01 05:34:16 +0000

    hh

--- Analytics-Week4/Analytics/models/staging/stg_fhv_tripdata.sql
+++ Analytics-Week4/Analytics/models/staging/stg_fhv_tripdata.sql
@@ -1,23 +1,18 @@
 {{
     config(
-        materialized='table'
+        materialized='view'
     )
 }}
 
-with fhv_tripdata as 
-(
-  select *
-  from {{ source('staging','fhv_tripdata_2019_ext`') }}
-)
-select
-
-    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,   
-    TIMESTAMP(CAST(fhv_tripdata.pickup_datetime AS STRING)) AS pickup_datetime,
-    TIMESTAMP(CAST(fhv_tripdata.dropOff_datetime AS STRING)) AS dropoff_datetime,
-    dispatching_base_num , SR_Flag , Affiliated_base_number
-from  fhv_tripdata
-
+select 
+     dispatching_base_num  ,
+    cast(PUlocationID as integer ) as Pickup_locationid ,
+    cast(DOlocationID  as integer) as dropoff_locationid,
+    CAST(pickup_datetime AS TIMESTAMP)) AS pickup_datetime,
+    CAST(dropOff_datetime AS TIMESTAMP)) AS dropoff_datetime,
+   , SR_Flag 
+from  {{ source('staging','fhv_tripdata`') }}
+where extract( year from Pickup_locationid ) = 2019
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
